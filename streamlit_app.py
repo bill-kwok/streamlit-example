77ee1a993e2323b7bce2d38c83ed80aa00dc040b
bill-kwok <102035359+bill-kwok@users.noreply.github.com>
2022-03-21 14:53:43 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,10 @@ import streamlit as st
 st.title("Hello DS4")
 st.header("DS4 > DS3 == True")
 
+num1 = st.text_input("First number: ")
+num2 = st.text_input("Second number: ")
+st.text(int(num1)+int(num2))
+
 click = st.button("Snow")
 if click:
   st.snow()
